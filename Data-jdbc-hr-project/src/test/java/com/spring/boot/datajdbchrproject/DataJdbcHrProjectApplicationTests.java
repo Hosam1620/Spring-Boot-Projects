01b2fe1d7728e1dff3723548ee015a25e1066f35
Hosam1620 <hosammohamed1620@gmail.com>
2025-11-27 14:00:22 +0200

    Data jdbc project

---
+++ Data-jdbc-hr-project/src/test/java/com/spring/boot/datajdbchrproject/DataJdbcHrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.datajdbchrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJdbcHrProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
